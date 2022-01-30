fd26878413ad4d9c21d8f5f65c7f363cf0ddfa04
asahi0711 <as8184251@gmail.com>
2022-01-30 17:40:52 +0900

    コミット1

---
+++ Console WebCamera.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_world455d.lib")
+#else
+#pragma comment(lib, "opencv_world455.lib")
+#endif
+
+int main()
+{
+	const char* windowName = "Image";
+	
+
+	cv::VideoCapture cap(0);
+	if (!cap.isOpened())//カメラデバイスが正常にオープンしたか確認．
+	{
+		return -1;
+	}
+
+	while (1)
+	{
+		cv::Mat img;
+		cap >> img;
+
+		// 必要に応じてここに画像処理
+		cv::Mat gray;
+		cv::Mat binary;
+		cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+		cv::threshold(gray, binary, 127, 255, cv::THRESH_BINARY);
+
+		cv::imshow(windowName, binary);//画像を表示．
+
+		int key = cv::waitKey(1);
+		if (key == 113)//qボタンが押されたとき
+		{
+			break;//whileループから抜ける．
+		}
+	}
+	cv::destroyAllWindows();
+
+	return(0);
+}
